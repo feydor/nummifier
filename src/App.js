83a215b73a67ce91ce5bc6c849348c6295f6bc0f
feydor <vreyes85739@outlook.com>
2020-11-10 21:11:46 -0700

    Added DigitalReductionBars functionality

--- src/App.js
+++ src/App.js
@@ -35,9 +35,11 @@ let currentGematria = "AQ"; // TODO: Refactor into react state variable (to swit
 
 // neoroman nummification
 // AQ, GoN, EQ...
+// input: neoroman encoded string
+// output: number
 function gematria(str) {
     let iterations = [];
-    let strArr = [...str]
+    let strArr = [...str];
     let finalReduction = strArr.reduce((acc, curr) => {
         let currReduction = AQ.find(
             (gematria) => gematria[0].toLowerCase() === curr.toLowerCase()
@@ -45,14 +47,15 @@ function gematria(str) {
         iterations.push(acc + currReduction[1]);
         return acc + currReduction[1];
     }, 0);
-    console.log(finalReduction);
+    // console.log(finalReduction);
     return finalReduction;
-    // return ([ finalReduction, iterations ]);
 }
 
-// reduces a two digit (or lower) number by summation
+// reduces an n digit number by summation
 // ex: 78 => 15
-// ex: 9 => 9
+// ex: 9999 => 36
+// input: n digit number
+// output: n-1 digit number
 function reduce(num) {
     let arr = [];
     while (num !== 0) {
@@ -60,58 +63,83 @@ function reduce(num) {
         arr.push(currDigit);
         num = Math.trunc(num / 10);
     }
-    return arr.reduce((acc, curr) => { return acc + curr });
+    return arr.reduce((acc, curr) => {
+        return acc + curr;
+    });
 }
 
+// input: neoroman encoded string
+// output: array containing a complete digital reduction [n, n-1, n-2, ...]
 function toAQ(query) {
     let initialNummificated = gematria(query);
     // reduce until single digit, keep track of reductions
     let reduction = initialNummificated; // initial value
     let reductionHistory = [initialNummificated];
     while (reduction >= 10) {
-      reduction = reduce(reduction);
-      reductionHistory.push(reduction);
+        reduction = reduce(reduction);
+        reductionHistory.push(reduction);
     }
     console.log(reductionHistory);
+    return reductionHistory;
 }
 
-// handler function for QueryBar
-// extracts input from onChange event, strips it of invalid characters,
-// and calls the appropriate gematric method
-function handleQuery(e) {
-    let query = e.target.value;
-    query = query.match(/[A-Z]/ig);
-    // console.log(query);
-    if (query !== null && currentGematria === "AQ") toAQ(query);
-}
+function QueryBar(props) {
+    // handler function for QueryBar
+    // extracts input from onChange event, strips it of invalid characters,
+    // and calls the appropriate gematric method
+    function handleQuery(e) {
+        let query = e.target.value;
+        query = query.match(/[A-Z]/gi);
+        // console.log(query);
+        let reductions = [];
+        if (query !== null && currentGematria === "AQ")
+            reductions = toAQ(query);
 
-function QueryBar() {
+        if (reductions.length !== 0) {
+            props.setReductions(reductions);
+        } else {
+            console.log("Gematric encoding failed!");
+        }
+    }
+
     return (
         <div id="QueryBar">
+            <h2>Query:</h2>
             <input type="text" id="query" name="query" onChange={handleQuery} />
         </div>
     );
 }
 
 function Glossary() {
-    return <div id="Glossary"></div>;
+    return (
+        <div id="Glossary">
+            <h2>Hyperglossolalary</h2>
+        </div>
+    );
 }
 
-function DigitalReductionBar() {
-    return <div id="DigitalReductionBar"></div>;
+function DigitalReductionBars(props) {
+    const reductions = props.reductions;
+    return (
+        <div id="DigitalReductionBars">
+            <h2>Digital Reduction:</h2>
+            {reductions.map((curr, idx) => (
+                <div key={idx} class="DigitalReductionBar">
+                    {curr}
+                </div>
+            ))}
+        </div>
+    );
 }
 
 function App() {
+    const [reductions, setReductions] = useState([]);
 
     return (
         <div className="App">
             <h1 id="title">Abysmal Nummification of the Signifier</h1>
-            <QueryBar />
-            <DigitalReductionBar />
-            <DigitalReductionBar />
-            <DigitalReductionBar />
-            <DigitalReductionBar />
-            <DigitalReductionBar />
+            <QueryBar setReductions={setReductions} />
+            <DigitalReductionBars reductions={reductions} />
             <Glossary />
         </div>
     );
