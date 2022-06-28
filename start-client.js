43d14a566a6b530a652233493e97809f0a716e49
feydor <vreyes85739@outlook.com>
2022-06-27 23:40:13 -0500

    remove old files

--- start-client.js
+++
@@ -1,4 +0,0 @@
-/* start-client.js - client/ start script for cross-os compatability */
-const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
