722c18c9ec49dd3a4a618d1dfc4a7d3d9d3785b9
feydor <vreyes85739@outlook.com>
2022-07-29 21:54:52 -0500

    add bun as transpiler; create working roman numeral procedure.

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,16 @@
 import Image from '../node_modules/next/image';
 import styles from './layout.module.css';
+import romanize from '../lib/rome';
 
 export default function Layout({ children }) {
     const date = new Date();
-    const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()]
+    const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
     return (
     <div>
         <header className={styles.Header}>
-
             <Image src="/images/multi35.jpg" width={35} height={35}/>
-            <p>{month+1}.{day}.{year}</p>
+            <p>{romanize(month+1)}.{romanize(day)}.{romanize(year)}</p>
             <p><em>INDEX</em></p>
-            
         </header>
         {children}
     </div>
