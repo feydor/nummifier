70e92ef05d7ed5e67cb8f3e12d9e2d4ea13a9f3e
feydor <vreyes85739@outlook.com>
2022-07-29 23:53:16 -0500

    Fix compile error in jsx

--- components/current-date.tsx
+++ components/current-date.tsx
@@ -5,7 +5,7 @@ export default function CurrentDate() {
     const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
     return (
         <Fragment>
-            <p style={{fontSize: "18px"}, {marginTop: "5px"}}>{romanize(month+1)}.{romanize(day)}.{romanize(year)}</p>
+            <p style={[{fontSize: "18px"}, {marginTop: "5px"}]}>{romanize(month+1)}.{romanize(day)}.{romanize(year)}</p>
         </Fragment>
     );
 }
